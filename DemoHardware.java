6862df82f9293a7189a6c1b53f164c42f0eda753
blitzbeta88 <55894468+blitzbeta88@users.noreply.github.com>
2023-07-12 21:28:31 -0400

    Add files via upload

--- DemoHardware.java
+++ DemoHardware.java
@@ -1,7 +1,5 @@
-//I have no clue what I'm supposed to do.
-package org.firstinspires.ftc.teamcode.FIRST_Robotics;
+package org.firstinspires.ftc.teamcode.GearsOfFire516;
 
-//Imports motor thingy, I have not read the documentation yet.
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,47 +7,36 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.Range;
 
-
-//I know how to do this.
+//Establishing your class
 public class DemoHardware {
 
-    //What?! I've never done this before!
+    //Define motors, servos, gyro, sensors
     public DcMotor demoWheel1;
-    //Well, it's a new experience. Let's hope it doesn't get any worse.
 
-    //Wheels,
     public DcMotor demoWheel2;
 
-    //Servo, alright. This is familiar
     public Servo demoServo;
 
-    //Tf is this
     public BNO055IMU gyro;
-    //Ohhh, we're getting a gyro from...Arby's? If it were so easy.
 
-    //Low maximum speed, don't want to confuse the driver for the test.
+    //Control speed of robot easily by changing this variable
     public static double maxSpeed = 1;
 
-    //WHAT?!
+    //Create an instance of your hardware class
     private static DemoHardware myInstance = null;
-    public static DemoHardware getInstance(){
-
-        //What am I doing what am I doing what am I doing
-        if (myInstance == null) {
+    public static DemoHardware getInstance() {
+        if(myInstance == null) {
             myInstance = new DemoHardware();
         }
-        //Returns something.
         return myInstance;
     }
 
-    //I am doing an absurd amount of comments. Ain't stoppin' now!
-
-    //I haven't a clue what I'm doing.
-
-    //uh oh
+    //Initialize all hardware
     public void init(HardwareMap hwMap) {
+
+        //Initialize motor
         try {
-            demoWheel1 = hwMap.get(DcMotor.class,"demoWheel");
+            demoWheel1 = hwMap.get(DcMotor.class, "demoWheel1");
             demoWheel1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             demoWheel1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             demoWheel1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -58,9 +45,9 @@ public class DemoHardware {
         catch(Exception p_exception) {
             demoWheel1 = null;
         }
-        //What does 99 percent of this mean???
+
         try {
-            demoWheel2 = hwMap.get(DcMotor.class, "demoWheel");
+            demoWheel2 = hwMap.get(DcMotor.class, "demoWheel2");
             demoWheel2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             demoWheel2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             demoWheel2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -69,44 +56,46 @@ public class DemoHardware {
         catch(Exception p_exception) {
             demoWheel2 = null;
         }
-        //Wth am I doing?
-        try{
+
+        //Initialize gyro
+        try {
             gyro = hwMap.get(BNO055IMU.class, "gyro");
             BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
             parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
             parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
             parameters.loggingEnabled = true;
             parameters.loggingTag = "gyro";
-            parameters.accelerationIntegrationAlgorithm =  new JustLoggingAccelerationIntegrator();
+            parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
             gyro.initialize(parameters);
-            //Brain aneurysm;
         }
         catch (Exception p_exception) {
             gyro = null;
         }
-        //Je ne comprends pas.
+
+        //Initialize servo
         try {
             demoServo = hwMap.get(Servo.class, "demoServo");
-
-        } catch (Exception p_exception) {
+        } catch(Exception p_exception) {
             demoServo = null;
         }
-        //Routes power to the wheels.
+
     }
-    public void setPower(double wheelDemo, double wheelDemo2){
+
+    //Method to setPower to all wheels at once
+    public void setPower(double wheelDemo1, double wheelDemo2) {
         if (demoWheel1 != null) {
-            demoWheel1.setPower(Range.clip(wheelDemo, -maxSpeed, maxSpeed));
+            demoWheel1.setPower(Range.clip(wheelDemo1, -maxSpeed, maxSpeed));
         }
-        if(demoWheel2 != null){
+        if (demoWheel2 != null) {
             demoWheel2.setPower(Range.clip(wheelDemo2, -maxSpeed, maxSpeed));
         }
     }
-    //Routes power to the servos. LIKE WE USED ANY HAHAHAHAHA jk
-    public void demoServo(double servoDemo){
-        if (demoServo != null){
+
+    //Method to easily set power to servo
+    public void demoServo(double servoDemo) {
+        if (demoServo != null) {
             demoServo.setPosition(servoDemo);
-            //Imagine if we used motors for our claw...
         }
     }
+
 }
-//If only Bong Water was here to share their wisdom...
--- DemoT.java
+++ DemoT.java
@@ -1,48 +1,60 @@
-package org.firstinspires.ftc.teamcode.FIRST_Robotics;
+package org.firstinspires.ftc.teamcode.GearsOfFire516;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-@TeleOp (name = "TeleOp")
+//See Autonomous program for more in depth comments
+@TeleOp(name = "TeleOp")
 public class DemoT extends LinearOpMode {
+
+
     private ElapsedTime runtime = new ElapsedTime();
     DemoHardware robot = DemoHardware.getInstance();
 
-    public void runOpMode(){
+    public void runOpMode() {
         robot.init(hardwareMap);
+
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
-        if (robot.demoWheel1 != null ) {
+        //Turn off encoders in case something gets unplugged
+        if (robot.demoWheel1 != null) {
             robot.demoWheel1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
         }
-        if(robot.demoWheel2 != null){
-            robot.demoWheel2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-
+        if (robot.demoWheel2 != null) {
+            robot.demoWheel2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         }
+
         DemoHardware.maxSpeed = 1;
+
         waitForStart();
 
+        //Booleans to ensure that buttons are only counted once when pressed
         boolean pressinga = false;
-        while(opModeIsActive()){
+
+        while (opModeIsActive()) {
+
+            //Robot directionals
             double movement = gamepad1.left_stick_y;
             double turning = -gamepad1.right_stick_x;
 
             double left = movement + turning;
-            double right =  movement - turning;
+            double right = movement - turning;
+
             double max = Math.max(Math.abs(left), Math.abs(right));
             if (max > 1.0) {
                 left /= max;
-                right /=  max;
-
+                right /= max;
             }
+
             robot.demoWheel1.setPower(left);
             robot.demoWheel2.setPower(right);
 
-            if (gamepad1.a && !pressinga ){
+            //Gamepad button example
+            if ((gamepad1.a && !pressinga)){
+                //Enter whatever function you want here
                 pressinga = true;
             } else if (!gamepad1.a) {
                 pressinga = false;
